e5d11f40aa3d9b77c92271d731284907e61385d0
Rolando220 <roly22216@gmail.com>
2026-06-12 12:40:31 +0200

    quarter_car model implementation

---
+++ Identificazione e stima/quarter_car_parameters.m
@@ -0,0 +1,18 @@
+clear all
+close all
+clc
+
+parameters.m_s = 350;
+parameters.m_u = 50;
+parameters.ks0 = 20000;
+parameters.alpha = 10^5;
+parameters.bs = 1500;
+parameters.kt = 180000;
+parameters.bt = 150;
+
+omega_n1 = 80;       
+zeta_1 = 0.7;        
+tau_1 = 0.005;       
+
+tau_a = 0.025;       
+tau_2 = 0.010;       
